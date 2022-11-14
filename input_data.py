db0afb26631d7ef39027e154dd386319f40d7a38
Fingerlink <niger-23@yandex.ru>
2022-11-14 13:08:55 +0700

    add new function

--- input_data.py
+++ input_data.py
@@ -1,6 +1,10 @@
 from easygui import *
 import easygui
 
-def user_input():
+def user_input_ration():
+    text = enterbox(msg='Введите выражение, которое будем считать!', title=' ', default='', strip=True, image=None, root=None)
+    return text
+
+def user_input_complex():
     text = enterbox(msg='Введите выражение, которое будем считать!\n Для комплексного числа используем только i или j!', title=' ', default='', strip=True, image=None, root=None)
     return text
